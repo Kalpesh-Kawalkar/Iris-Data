5629a25c68e108102885f758f3d771e423d99f9e
Kalpesh Kawalkar <102687995+Kalpesh-Kawalkar@users.noreply.github.com>
2023-03-22 23:07:53 +0530

    Create Iris.R

---
+++ Iris.R
@@ -0,0 +1,14 @@
+library (datasets)
+data(iris)
+View(iris)
+
+unique(iris$Species)
+
+library(GGally)
+ggpairs(iris, mapping=ggplot2::aes(colour = Species))
+
+
+#This gives us a lot of information for a single line of code. 
+#First, we can see the data distributions per column and species on the diagonal. 
+#Then we see all the pair-wise scatter plots on the tiles left to the diagonal, again segregated by color. 
+#It is, for example, obvious that a line can be drawn to separate setosa against versicolor and virginica. 
